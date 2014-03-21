821eaa79841d638a1d13595df015d060b4592f22
Burzum302 <Burzum302@gmail.com>
2014-03-21 10:42:54 -0400

    sfuihnqokmwple

---
+++ Runner/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public bool jumping;
+	public float acceleration;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(jumping == false && acceleration <= 8)
+
+		transform.rigidbody.AddForce (800*Time.deltaTime, 0, 0, ForceMode.Acceleration);
+		acceleration = rigidbody.velocity.x;
+		if (Input.GetKeyDown ("s") && jumping == false) {
+			transform.localScale = new Vector3(1f,0.5f,1f);
+		}
+		if (Input.GetKeyUp ("s") && jumping == false) {
+			transform.localScale = new Vector3(1f,1f,1f);
+		}
+	}
+	void FixedUpdate(){
+		if(Input.GetKeyDown("w") && jumping == false){
+			transform.rigidbody.AddForce(Vector3.up*300*Time.deltaTime,ForceMode.VelocityChange);
+			jumping = true;
+		}
+
+	}
+	void OnCollisionEnter(Collision C){
+		jumping = false;
+		if (C.gameObject.name == "Log" || C.gameObject.name == ("Door")) {
+			Application.LoadLevel("Level1");
+		}
+	}
+}
