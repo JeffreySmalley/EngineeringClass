d284059b720e32473b425a2277e2be9460842d09
Jeffrey Smalley <cloudwomega@gmail.com>
2014-03-30 06:22:17 -0400

    Changed Player from box collider and rigid body to using Character Controller
    check that all your levels still work.
    
    PS lost all my work on the enemies due to user profile corruption, took
    me a night to restore to a usable state, will get basic enemies done by
    Wednesday not Monday to compensate

--- Runner/Assets/Scripts/Player/Player.cs
+++ Runner/Assets/Scripts/Player/Player.cs
@@ -6,14 +6,28 @@ public class Player : MonoBehaviour {
 	public float acceleration;
 	public static int lives;
 	public static int score;
-	// Use this for initialization
-	void Start () 
-	{
 
+	private float gravity = 10.0f;
+
+	private Vector3 motion = Vector3.zero;
+
+	// Use this for initialization
+	private CharacterController controller
+	{
+		get
+		{
+			return GetComponent<CharacterController>();
+		}
+	}
+	void Start () 
+	{		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		/*
 		if(jumping == false && acceleration <= 8)
 
 		transform.rigidbody.AddForce (800*Time.deltaTime, 0, 0, ForceMode.Acceleration);
@@ -24,21 +38,29 @@ public class Player : MonoBehaviour {
 		if (Input.GetKeyUp ("s") && jumping == false) {
 			transform.localScale = new Vector3(1f,1f,1f);
 		}
+		*/
+		motion.x = 800.0f*Time.deltaTime;
+		motion.y -= gravity * Time.deltaTime;
+		controller.Move (motion * Time.deltaTime);
 	}
 	void FixedUpdate(){
-		if(Input.GetKeyDown("w") && jumping == false){
-			transform.rigidbody.AddForce(Vector3.up*320*Time.deltaTime,ForceMode.VelocityChange);
-			jumping = true;
-		}
 
+		if(Input.GetKeyDown("w") && /*jumping == false*/ controller.isGrounded){
+			motion.y = 10.0f;
+			/*
+			 transform.rigidbody.AddForce(Vector3.up*320*Time.deltaTime,ForceMode.VelocityChange);
+			jumping = true;
+			*/
+		}
 	}
 	void OnCollisionEnter(Collision C){
+		/*
 		jumping = false;
 		if (C.gameObject.tag == "Obstacle" && transform.localPosition.y < 2){
 			Application.LoadLevel(Application.loadedLevel);
 		}
 		if(C.gameObject.tag == "Exit"){
 			Application.LoadLevel(Application.loadedLevel+1);
-		}
+		}*/
 	}
 }
